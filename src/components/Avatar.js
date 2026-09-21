f864a54d5095a44242ef97dae53eb4caed98a69c
MuriloTBS <smnogueira@proton.me>
2026-09-20 21:00:04 -0300

    feat: cria componente Avatar

---
+++ src/components/Avatar.js
@@ -0,0 +1,21 @@
+import { Image, StyleSheet } from 'react-native';
+import { colors } from '../theme/colors';
+
+// Foto de perfil redonda. `size` controla o tamanho em pixels.
+export default function Avatar({ uri, size = 40 }) {
+  return (
+    <Image
+      source={{ uri }}
+      style={[
+        styles.avatar,
+        { width: size, height: size, borderRadius: size / 2 },
+      ]}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  avatar: {
+    backgroundColor: colors.border,
+  },
+});
